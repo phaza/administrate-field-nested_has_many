a10b525ecdce405daa5a4a8b87e7b2a9e6032e51
Peter Haza <peter.haza@gmail.com>
2016-05-13 11:16:00 +0200

    Try this with rails 5

--- lib/administrate/field/nested_has_many.rb
+++ lib/administrate/field/nested_has_many.rb
@@ -7,7 +7,7 @@ require "cocoon"
 module Administrate
   module Field
     class NestedHasMany < Administrate::Field::HasMany
-      VERSION = "0.1.0"
+      VERSION = "0.1.1"
 
       class Engine < ::Rails::Engine
         Administrate::Engine.add_javascript "administrate-field-nested_has_many/application"
